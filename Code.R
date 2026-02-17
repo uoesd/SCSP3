4a9904b43f9a129ff0df609d38a13a7231b7578f
yixuan798 <yixuany798@gmail.com>
2026-02-17 18:16:22 +0000

    Bonus: summary table, (e.g.human vs gemini, test chatgpt)

--- Code.R
+++ Code.R
@@ -14,6 +14,123 @@ build_dataset <- function(features_list) {
   list(X = X, y = y)
 }
 
+
+loadCorpus <- function(filedir,featureset="functionwords",maxauthors=Inf) {
+  authornames <- list.files(filedir)
+  booknames <- list()
+  features <- list()
+  count <- 0
+  
+  for (i in 1:length(authornames)) {
+    #print(i)
+    if (count >= maxauthors) {break}
+    files <- list.files(sprintf("%s%s/",filedir,authornames[i]))
+    if (length(files)==0) {next}
+    
+    firstbook <- FALSE
+    booknames[[i]] <- character()
+    for (j in 1:length(files)) {
+      path <- sprintf("%s%s/%s",filedir,authornames[i],files[j])
+      
+      fields <- strsplit(files[j],split=' --- ')[[1]]  
+      
+      if (sprintf("%s.txt",featureset) == fields[2]) {
+        booknames[[i]] <- c(booknames[[i]], fields[1])
+        count <- count+1
+        M <- as.matrix(read.csv(path,sep=',',header=FALSE))  
+        if (firstbook == FALSE) {
+          firstbook <- TRUE
+          features[[i]] <- M
+        } else {
+          features[[i]]  <- rbind(features[[i]],M)
+        }
+        
+      }
+    }
+  }
+  return(list(features=features,booknames=booknames,authornames=authornames))
+}
+
+discriminantCorpus <- function(traindata, testdata) {
+  thetas <- NULL
+  preds <- NULL
+  
+  #first learn thea model for each aauthor
+  for (i in 1:length(traindata)) {
+    words <- apply(traindata[[i]],2,sum)
+    
+    #some words might never occur. This will be a problem since it will mean the theta for this word is 0, which means the likelihood will be 0 if this word occurs in the training set. So, we force each word to occur at leats once
+    inds <- which(words==0) 
+    if (length(inds) > 0) {words[inds] <- 1}
+    thetas <- rbind(thetas, words/sum(words))
+  }
+  
+  #now classify
+  for (i in 1:nrow(testdata)) {
+    probs <- NULL
+    for (j in 1:nrow(thetas)) {
+      probs <- c(probs, dmultinom(testdata[i,],prob=thetas[j,],log=TRUE))
+    }
+    preds <- c(preds, which.max(probs))
+  }
+  return(preds)
+}
+
+
+myKNN <- function(traindata, testdata, trainlabels, k=1) {
+  if (mode(traindata) == 'numeric' && !is.matrix(traindata)) {
+    traindata <- matrix(traindata,nrow=1)
+  }
+  if (mode(testdata) == 'numeric' && !is.matrix(testdata)) {
+    testdata <- matrix(testdata,nrow=1)
+  }
+  
+  mus <- apply(traindata,2,mean) 
+  sigmas <- apply(traindata,2,sd)
+  
+  for (i in 1:ncol(traindata)) {
+    traindata[,i] <- (traindata[,i] - mus[i])/sigmas[i]
+  }
+  
+  for (i in 1:ncol(testdata)) {
+    testdata[,i] <- (testdata[,i]-mus[i])/sigmas[i]
+  }
+  
+  preds <- knn(traindata, testdata, trainlabels, k)
+  return(preds)
+}
+
+discriminantCorpus <- function(traindata, testdata) {
+  thetas <- NULL
+  preds <- NULL
+  
+  #first learn thea model for each aauthor
+  for (i in 1:length(traindata)) {
+    words <- apply(traindata[[i]],2,sum)
+    
+    #some words might never occur. This will be a problem since it will mean the theta for this word is 0, which means the likelihood will be 0 if this word occurs in the training set. So, we force each word to occur at leats once
+    inds <- which(words==0) 
+    if (length(inds) > 0) {words[inds] <- 1}
+    thetas <- rbind(thetas, words/sum(words))
+  }
+  
+  #now classify
+  for (i in 1:nrow(testdata)) {
+    probs <- NULL
+    for (j in 1:nrow(thetas)) {
+      probs <- c(probs, dmultinom(testdata[i,],prob=thetas[j,],log=TRUE))
+    }
+    preds <- c(preds, which.max(probs))
+  }
+  return(preds)
+}
+
+
+
+
+
+
+
 # Loading data
 M <- loadCorpus("Data/FunctionWords/", "frequentwords")
 authors <- M$authornames
@@ -338,30 +455,7 @@ hist(log_diff, main="Gemini log-likelihood difference", xlab="log p(Human) - log
 abline(v=0)
 
 
-# method 2??
-human_counts <- colSums(features[[human_index]])
-gpt_counts   <- colSums(features[[gpt_index]])
 
-trainset <- rbind(human_counts, gpt_counts)
-
-# modelling
-C <- dim(trainset)[1]
-pis <- rep(1/C, C)
-
-thetas <- matrix(0, nrow=C, ncol=ncol(trainset))
-
-for (i in 1:C) {
-  thetas[i,] <- trainset[i,] / sum(trainset[i,])
-}
-
-
-posterior <- numeric(C)
-
-for(i in 1:C){
-  posterior[i] <- log(pis[i]) + dmultinom(testdata, prob=thetas[i,], log=TRUE)
-}
-
-which.max(posterior)
 
 
 
@@ -394,3 +488,30 @@ table(pred_l_h_g)
 # Human vs Llama: Test on Gemini
 pred_l_h_ge <- discriminantCorpus(list(Llama, Human), Gemini)
 table(pred_l_h_ge)
+
+
+
+summary_table <- data.frame(
+  Train = c("GPT vs Human",
+            "GPT vs Human",
+            "Gemini vs Human",
+            "Gemini vs Human",
+            "Llama vs Human",
+            "Llama vs Human"),
+  Test = c("Gemini",
+           "Llama",
+           "GPT",
+           "Llama",
+           "GPT",
+           "Gemini"),
+  Prop_Class1 = c(
+    prop.table(table(pred_g_h_ge))[1],
+    prop.table(table(pred_g_h_l))[1],
+    prop.table(table(pred_ge_h_g))[1],
+    prop.table(table(pred_ge_h_l))[1],
+    prop.table(table(pred_l_h_g))[1],
+    prop.table(table(pred_l_h_ge))[1]
+  )
+)
+
+summary_table
