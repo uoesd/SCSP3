142d8b00b65d404413707dcc607ddbfb15050a87
ZhangKunning <s2274549@ed.ac.uk>
2026-02-18 03:53:41 +0000

    1

--- Code.R
+++ Code.R
@@ -14,123 +14,6 @@ build_dataset <- function(features_list) {
   list(X = X, y = y)
 }
 
-
-loadCorpus <- function(filedir,featureset="functionwords",maxauthors=Inf) {
-  authornames <- list.files(filedir)
-  booknames <- list()
-  features <- list()
-  count <- 0
-  
-  for (i in 1:length(authornames)) {
-    #print(i)
-    if (count >= maxauthors) {break}
-    files <- list.files(sprintf("%s%s/",filedir,authornames[i]))
-    if (length(files)==0) {next}
-    
-    firstbook <- FALSE
-    booknames[[i]] <- character()
-    for (j in 1:length(files)) {
-      path <- sprintf("%s%s/%s",filedir,authornames[i],files[j])
-      
-      fields <- strsplit(files[j],split=' --- ')[[1]]  
-      
-      if (sprintf("%s.txt",featureset) == fields[2]) {
-        booknames[[i]] <- c(booknames[[i]], fields[1])
-        count <- count+1
-        M <- as.matrix(read.csv(path,sep=',',header=FALSE))  
-        if (firstbook == FALSE) {
-          firstbook <- TRUE
-          features[[i]] <- M
-        } else {
-          features[[i]]  <- rbind(features[[i]],M)
-        }
-        
-      }
-    }
-  }
-  return(list(features=features,booknames=booknames,authornames=authornames))
-}
-
-discriminantCorpus <- function(traindata, testdata) {
-  thetas <- NULL
-  preds <- NULL
-  
-  #first learn thea model for each aauthor
-  for (i in 1:length(traindata)) {
-    words <- apply(traindata[[i]],2,sum)
-    
-    #some words might never occur. This will be a problem since it will mean the theta for this word is 0, which means the likelihood will be 0 if this word occurs in the training set. So, we force each word to occur at leats once
-    inds <- which(words==0) 
-    if (length(inds) > 0) {words[inds] <- 1}
-    thetas <- rbind(thetas, words/sum(words))
-  }
-  
-  #now classify
-  for (i in 1:nrow(testdata)) {
-    probs <- NULL
-    for (j in 1:nrow(thetas)) {
-      probs <- c(probs, dmultinom(testdata[i,],prob=thetas[j,],log=TRUE))
-    }
-    preds <- c(preds, which.max(probs))
-  }
-  return(preds)
-}
-
-
-myKNN <- function(traindata, testdata, trainlabels, k=1) {
-  if (mode(traindata) == 'numeric' && !is.matrix(traindata)) {
-    traindata <- matrix(traindata,nrow=1)
-  }
-  if (mode(testdata) == 'numeric' && !is.matrix(testdata)) {
-    testdata <- matrix(testdata,nrow=1)
-  }
-  
-  mus <- apply(traindata,2,mean) 
-  sigmas <- apply(traindata,2,sd)
-  
-  for (i in 1:ncol(traindata)) {
-    traindata[,i] <- (traindata[,i] - mus[i])/sigmas[i]
-  }
-  
-  for (i in 1:ncol(testdata)) {
-    testdata[,i] <- (testdata[,i]-mus[i])/sigmas[i]
-  }
-  
-  preds <- knn(traindata, testdata, trainlabels, k)
-  return(preds)
-}
-
-discriminantCorpus <- function(traindata, testdata) {
-  thetas <- NULL
-  preds <- NULL
-  
-  #first learn thea model for each aauthor
-  for (i in 1:length(traindata)) {
-    words <- apply(traindata[[i]],2,sum)
-    
-    #some words might never occur. This will be a problem since it will mean the theta for this word is 0, which means the likelihood will be 0 if this word occurs in the training set. So, we force each word to occur at leats once
-    inds <- which(words==0) 
-    if (length(inds) > 0) {words[inds] <- 1}
-    thetas <- rbind(thetas, words/sum(words))
-  }
-  
-  #now classify
-  for (i in 1:nrow(testdata)) {
-    probs <- NULL
-    for (j in 1:nrow(thetas)) {
-      probs <- c(probs, dmultinom(testdata[i,],prob=thetas[j,],log=TRUE))
-    }
-    preds <- c(preds, which.max(probs))
-  }
-  return(preds)
-}
-
-
-
-
-
-
-
 # Loading data
 M <- loadCorpus("Data/FunctionWords/", "frequentwords")
 authors <- M$authornames
@@ -248,65 +131,6 @@ plot(pts,
 
 text(pts[,1], pts[,2], labels=authors)
 
-# DA
-
-# Test
-# Make test data matrix and test
-features_test_matrix <- do.call(rbind, features_test)
-pred_da_t <- discriminantCorpus(features_train, features_test_matrix)
-
-# Calculate accuracy and make confusion matrix
-truth_da_t <- rep(1:length(features_test), sapply(features_test, nrow))
-acc_da_t <- sum(pred_da_t  == truth_da_t) / length(truth_da_t)
-cm_da_t <- confusionMatrix(as.factor(pred_da_t), as.factor(truth_da_t))
-
-# Reporting table
-da_test_table <- data.frame(
-  Value = c(
-    cm_da_t$overall["Accuracy"],
-    cm_da_t$overall["Kappa"],
-    cm_da_t$byClass["Sensitivity"],
-    cm_da_t$byClass["Specificity"],
-    cm_da_t$byClass["Balanced Accuracy"],
-    cm_da_t$overall["AccuracyLower"],
-    cm_da_t$overall["AccuracyUpper"]
-  )
-)
-
-# LOOCV
-predictions_da_cv <- NULL
-truth_da_cv <- NULL
-
-for (i in 1:length(features_train)) {
-  for (j in 1:nrow(features_train[[i]])) {
-    
-    testdata_da_cv <- matrix(features_train[[i]][j,], nrow=1)
-    
-    traindata_da_cv <- features_train
-    traindata_da_cv[[i]] <- traindata_da_cv[[i]][-j,,drop=FALSE]
-    
-    pred_da_cv <- discriminantCorpus(traindata_da_cv, testdata_da_cv)
-    predictions_da_cv <- c(predictions_da_cv, pred_da_cv)
-    
-    truth_da_cv <- c(truth_da_cv, i)
-  }
-}
-
-# Calculate accuracy and make confusion matrix
-acc_da_cv  <- sum(predictions_da_cv  == truth_da_cv) / length(truth_da_cv)
-cm_da_cv  <- confusionMatrix(as.factor(predictions_da_cv), as.factor(truth_da_cv))
-
-# Reporting table
-da_loo_table  <- data.frame(
-  Value = c(
-    cm_da_cv$overall["Accuracy"],
-    cm_da_cv$overall["Kappa"],
-    cm_da_cv$byClass["Sensitivity"],
-    cm_da_cv$byClass["Specificity"],
-    cm_da_cv$byClass["Balanced Accuracy"]
-  )
-)
-
 # KNN
 
 # 10-fold CV analysis for k
@@ -346,33 +170,6 @@ for (k in 1:20) {
   sen_k_analysis[k] <- cm_k_analysis$byClass["Sensitivity"]
 }
 
-## LOOCV with k=2
-
-preds_knn_loo <- character(nrow(X))   # <-- FIXED TYPE
-
-for (i in seq_len(nrow(X))) {
-  
-  train_X <- X[-i, , drop = FALSE]
-  train_y <- y[-i]
-  test_X  <- X[i, , drop = FALSE]
-  
-  preds_knn_loo[i] <- as.character(myKNN(train_X, test_X, train_y, k = 2))
-}
-
-acc_knn_loo <- mean(preds_knn_loo == as.character(y))
-cm_knn_loo <- confusionMatrix(as.factor(preds_knn_loo), as.factor(y))
-
-# Reporting table
-knn_loo_table <- data.frame( 
-  Value = c(
-    cm_knn_loo$overall["Accuracy"],
-    cm_knn_loo$overall["Kappa"],
-    cm_knn_loo$byClass["Sensitivity"],
-    cm_knn_loo$byClass["Specificity"],
-    cm_knn_loo$byClass["Balanced Accuracy"]
-  )
-)
-
 # Reporting plot for k analysis
 
 k_vals <- 1:20
@@ -402,64 +199,93 @@ knn_cv_plot <- ggplot(plot_long, aes(x = k, y = Score,
     plot.title = element_text(hjust = 0.5, face = "bold")
   )
 
+## LOOCV with k=2
 
+preds_knn_loo <- character(nrow(X))   # <-- FIXED TYPE
 
-
-
-
-### Bonus
-
-M <- loadCorpus("Data/FunctionWords/", "frequentwords")
-authors <- M$authornames
-features <- M$features
-
-# find observations
-human_index  <- which(authors == "Human")
-gpt_index    <- which(authors == "GPT")
-gemini_index <- which(authors == "Gemini")
-
-# train human and gpt data
-train_data <- list(
-  features[[human_index]],
-  features[[gpt_index]]
-)
-
-
-# find parameters
-
-estimate_theta <- function(mat){
-  word_counts <- colSums(mat)
-  word_counts[word_counts==0] <- 1
-  theta <- word_counts / sum(word_counts)
-  return(theta)
+for (i in seq_len(nrow(X))) {
+  
+  train_X <- X[-i, , drop = FALSE]
+  train_y <- y[-i]
+  test_X  <- X[i, , drop = FALSE]
+  
+  preds_knn_loo[i] <- as.character(myKNN(train_X, test_X, train_y, k = 2))
 }
 
-theta_human <- estimate_theta(train_data[[1]])
-theta_gpt   <- estimate_theta(train_data[[2]])
+acc_knn_loo <- mean(preds_knn_loo == as.character(y))
+cm_knn_loo <- confusionMatrix(as.factor(preds_knn_loo), as.factor(y))
 
+# Reporting table
+knn_loo_table <- data.frame( 
+  Value = c(
+    cm_knn_loo$overall["Accuracy"],
+    cm_knn_loo$overall["Kappa"],
+    cm_knn_loo$byClass["Sensitivity"],
+    cm_knn_loo$byClass["Specificity"],
+    cm_knn_loo$byClass["Balanced Accuracy"]
+  )
+)
 
-# test Gemini data
-testdata <- features[[gemini_index]]
+# DA
 
+# LOOCV
+predictions_da_cv <- NULL
+truth_da_cv <- NULL
 
-# modelling with multivariate distributions
+for (i in 1:length(features_train)) {
+  for (j in 1:nrow(features_train[[i]])) {
+    
+    testdata_da_cv <- matrix(features_train[[i]][j,], nrow=1)
+    
+    traindata_da_cv <- features_train
+    traindata_da_cv[[i]] <- traindata_da_cv[[i]][-j,,drop=FALSE]
+    
+    pred_da_cv <- discriminantCorpus(traindata_da_cv, testdata_da_cv)
+    predictions_da_cv <- c(predictions_da_cv, pred_da_cv)
+    
+    truth_da_cv <- c(truth_da_cv, i)
+  }
+}
 
-log_h <- apply(testdata, 1, function(row) dmultinom(row, prob = theta_human, log = TRUE))
-log_g <- apply(testdata, 1, function(row) dmultinom(row, prob = theta_gpt,   log = TRUE))
+# Calculate accuracy and make confusion matrix
+acc_da_cv  <- sum(predictions_da_cv  == truth_da_cv) / length(truth_da_cv)
+cm_da_cv  <- confusionMatrix(as.factor(predictions_da_cv), as.factor(truth_da_cv))
 
-log_diff <- log_h - log_g
+# Reporting table
+da_loo_table  <- data.frame(
+  Value = c(
+    cm_da_cv$overall["Accuracy"],
+    cm_da_cv$overall["Kappa"],
+    cm_da_cv$byClass["Sensitivity"],
+    cm_da_cv$byClass["Specificity"],
+    cm_da_cv$byClass["Balanced Accuracy"]
+  )
+)
 
-#plot
-summary(log_diff)
-hist(log_diff, main="Gemini log-likelihood difference", xlab="log p(Human) - log p(GPT)")
-abline(v=0)
+# Test
+# Make test data matrix and test
+features_test_matrix <- do.call(rbind, features_test)
+pred_da_t <- discriminantCorpus(features_train, features_test_matrix)
 
+# Calculate accuracy and make confusion matrix
+truth_da_t <- rep(1:length(features_test), sapply(features_test, nrow))
+acc_da_t <- sum(pred_da_t  == truth_da_t) / length(truth_da_t)
+cm_da_t <- confusionMatrix(as.factor(pred_da_t), as.factor(truth_da_t))
 
+# Reporting table
+da_test_table <- data.frame(
+  Value = c(
+    cm_da_t$overall["Accuracy"],
+    cm_da_t$overall["Kappa"],
+    cm_da_t$byClass["Sensitivity"],
+    cm_da_t$byClass["Specificity"],
+    cm_da_t$byClass["Balanced Accuracy"],
+    cm_da_t$overall["AccuracyLower"],
+    cm_da_t$overall["AccuracyUpper"]
+  )
+)
 
-
-
-
-#######
+# Extra Research
 Gemini <- features[[1]]
 GPT <- features[[2]]
 Human <- features[[3]]
@@ -489,8 +315,6 @@ table(pred_l_h_g)
 pred_l_h_ge <- discriminantCorpus(list(Llama, Human), Gemini)
 table(pred_l_h_ge)
 
-
-
 summary_table <- data.frame(
   Train = c("GPT vs Human",
             "GPT vs Human",
@@ -504,28 +328,20 @@ summary_table <- data.frame(
            "Llama",
            "GPT",
            "Gemini"),
-  Prop_Class_AI = c(
-    prop.table(table(pred_g_h_ge))[1],
-    prop.table(table(pred_g_h_l))[1],
-    prop.table(table(pred_ge_h_g))[1],
-    prop.table(table(pred_ge_h_l))[1],
-    prop.table(table(pred_l_h_g))[1],
-    prop.table(table(pred_l_h_ge))[1]
+  Accuracy = c(prop.table(table(pred_g_h_ge))[1],
+               prop.table(table(pred_g_h_l))[1],
+               prop.table(table(pred_ge_h_g))[1],
+               prop.table(table(pred_ge_h_l))[1],
+               prop.table(table(pred_l_h_g))[1],
+               prop.table(table(pred_l_h_ge))[1])
   )
-)
 
+summary_table$Accuracy <- sprintf("%.3f", summary_table$Accuracy)
 summary_table
 
-
-#
-library(ggplot2)
-
 ggplot(summary_table,
-       aes(x=Test, y=Prop_Class_AI, group=Train, color=Train)) +
-  geom_line(size=1.2) +(base_size=14)
-  geom_point(size=3) +
-  ylim(0,1) +
-  labs(title="Classification Proportion under Different Training Models",
-       x="Tested AI",
-       y="Proportion classified as AI class") +
-  theme_minimal(base_size = 14)
+       aes(x = Test, y = Accuracy, group = Train, color = Train)) +
+  geom_line(size = 1.2) +
+  geom_point(size = 3) +
+  labs(y = "Accuracy", title = "Accuracy if we use the “wrong” LLM to train the data") +
+  theme_minimal()
